6ca2a0dcbd134144f1cbd3802df988a96cb98a55
🦀 adia-dev <abdoudu78130@gmail.com>
2025-01-10 15:19:31 +0100

    ✨ Added metrics, balances, unique holders etc..

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import { EvmBatchProcessor } from "@subsquid/evm-processor";
 import { TypeormDatabase } from "@subsquid/typeorm-store";
 import * as usdtAbi from "./abi/usdt";
-import { Token, Transfer } from "./model";
+import { Token, Transfer, Balance, Metrics } from "./model";
 
 const address = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 const processor = new EvmBatchProcessor()
@@ -22,6 +22,9 @@ processor.run(db, async (ctx) => {
   if (ctx.blocks.length === 0) return;
 
   const transfers: Transfer[] = [];
+  const balances: Map<string, bigint> = new Map();
+  const holders = new Set<string>();
+
   const contract = new usdtAbi.Contract(ctx, ctx.blocks[0].header, address);
   await ctx.store.upsert(
     new Token({
@@ -36,6 +39,17 @@ processor.run(db, async (ctx) => {
   for (let block of ctx.blocks) {
     for (let log of block.logs) {
       const { from, to, value } = usdtAbi.events.Transfer.decode(log);
+
+      if (from !== "0x0000000000000000000000000000000000000000") {
+        balances.set(from, (balances.get(from) || 0n) - value);
+      }
+      balances.set(to, (balances.get(to) || 0n) + value);
+
+      if (from !== "0x0000000000000000000000000000000000000000") {
+        holders.add(from);
+      }
+      holders.add(to);
+
       transfers.push(
         new Transfer({
           id: log.id,
@@ -46,5 +60,25 @@ processor.run(db, async (ctx) => {
       );
     }
   }
+
+  const balanceEntities = Array.from(balances.entries()).map(
+    ([address, balance]) => {
+      return new Balance({
+        id: address,
+        tokenId: address,
+        balance,
+      });
+    },
+  );
+  await ctx.store.upsert(balanceEntities);
+
+  // TODO: add a global metrics id that stores every metrics
+  const metrics = new Metrics({
+    id: `token:${address}`,
+    totalTransfers: transfers.length,
+    uniqueHolders: holders.size,
+  });
+  await ctx.store.upsert([metrics]);
+
   await ctx.store.insert(transfers);
 });
