8dff0193d318aeb40206bb735466233051ca3961
🦀 adia-dev <abdoudu78130@gmail.com>
2025-01-10 12:28:05 +0100

    🔧 Generated the typeorm classes

---
+++ backend/src/model/generated/transfer.model.ts
@@ -0,0 +1,22 @@
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, StringColumn as StringColumn_, Index as Index_, BigIntColumn as BigIntColumn_} from "@subsquid/typeorm-store"
+
+@Entity_()
+export class Transfer {
+    constructor(props?: Partial<Transfer>) {
+        Object.assign(this, props)
+    }
+
+    @PrimaryColumn_()
+    id!: string
+
+    @Index_()
+    @StringColumn_({nullable: false})
+    from!: string
+
+    @Index_()
+    @StringColumn_({nullable: false})
+    to!: string
+
+    @BigIntColumn_({nullable: false})
+    value!: bigint
+}
